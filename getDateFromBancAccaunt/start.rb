87d13a9b595a662ef4233388b361cb66d1471f3f
SSerghei <serzh.shchepanovskiy@gmail.com>
2020-08-30 19:33:24 +0300

    Добавил классы

--- getDateFromBancAccaunt/start.rb
+++ getDateFromBancAccaunt/start.rb
@@ -1,9 +1,34 @@
 #autor: SergheiScepanovschi
-#ver 1.0
+#ver 3.0
+require 'date'
 require 'watir'
 require 'webdrivers'
 require 'faker'
 require 'rubygems'
+
+class Transaction
+  def initialize(_date, _description, _amount)
+    @date        = _date         #Дата
+    @description = _description  #описание
+    @amount      = _amount       #сумма
+  end
+end
+
+class Card
+  def initialize(_carrierName)
+    @carrierName        = _carrierName       #имя владельца
+  end
+end
+class Accaunt
+  def initialize(_nameAccaunt, _currency, _availableBalance, _classification,_card, _transaction )
+    @nameAccaunt      = _nameAccaunt      #имя
+    @currency         = _currency         #валюта
+    @availableBalance = _availableBalance #баланс
+    @classification   = _classification   #природа
+    @card             = _card             #карты
+    @transaction      = _transaction      #транзакции
+  end
+end
 
 browser = Watir::Browser.new :chrome
 #заходим на сайт
@@ -12,11 +37,5 @@ browser.goto "https://demo.bendigobank.com.au/banking/sign_in"
 browser.button(:name => "customer_type").click
 #копируем необходимые данные
 strct = browser.script(:id => "data").innertext
-#"Счета": [
-#        "имя": "аккаунт1",
-# card
-#        «валюта»: «MDL»,
-#        «баланс»: 300,22,
-#        "природа": "кредитная карта",
-#        "транзакции": []
+
 puts strct
