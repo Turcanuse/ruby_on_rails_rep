1a8d203a50b8375e7498e4997f9203964f4a23c3
SSerghei <serzh.shchepanovskiy@gmail.com>
2020-08-30 17:30:03 +0300

    Initial Commit

---
+++ getDateFromBancAccaunt/start.rb
@@ -0,0 +1,20 @@
+require 'watir'
+require 'webdrivers'
+require 'faker'
+require 'rubygems'
+
+browser = Watir::Browser.new :chrome
+#заходим на сайт
+browser.goto "https://demo.bendigobank.com.au/banking/sign_in"
+#кликаем на кнопку входа
+browser.button(:name => "customer_type").click
+#копируем необходимые данные
+strct = browser.script(:id => "data").innertext
+#"Счета": [
+#        "имя": "аккаунт1",
+# card
+#        «валюта»: «MDL»,
+#        «баланс»: 300,22,
+#        "природа": "кредитная карта",
+#        "транзакции": []
+puts strct
