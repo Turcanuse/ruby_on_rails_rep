dbc09809bcacd64c46688f70de67755833ba7d8c
SSerghei <serzh.shchepanovskiy@gmail.com>
2020-08-31 13:29:35 +0300

    добавил копирование подстроки с данными и её парсинг

--- getDateFromBancAccaunt/start.rb
+++ getDateFromBancAccaunt/start.rb
@@ -1,10 +1,11 @@
 #autor: SergheiScepanovschi
-#ver 3.1
+#ver 3.2
 require 'date'
 require 'watir'
 require 'webdrivers'
 require 'faker'
 require 'rubygems'
+require 'json'
 
 class Transaction
   def initialize(_date, _description, _amount)
@@ -42,6 +43,14 @@ browser.button(:name => "customer_type").click
 strct = browser.script(:id => "data").innertext
 
 puts strct
-
+#Находим данные
+pos1 = strct.rindex(/__DATA__/)
+# Выделяем строку с данными
+strct1 = strct.slice(pos1+10,13897) # я пока не знаю как получить положение третьего переноса строки
+puts strct1
+#здесь при парсинге JSON возникает ошибка из-за лишних символов в строке
+my_hash = JSON.parse(strct1)
+puts my_hash["name"] #пример того как можно получить значени из распарсенной строки
+# пример того как будет заполнятся оьект класса
 accaunt = Accaunt.new("DailyAcc", "MDL", 1213123123,"Debit")
 accaunt.exec
