bf10f7968e7363ece3553e54aae6dd94382353b9
SSerghei <serzh.shchepanovskiy@gmail.com>
2020-09-01 09:20:45 +0300

    Добавил автозаполнение некотрых полей класса из hesh json добавил проверочный метод печати заполненных данных

--- getDateFromBancAccaunt/start.rb
+++ getDateFromBancAccaunt/start.rb
@@ -35,15 +35,18 @@ class Accaunt
     @currency         = _currency         #валюта
     @availableBalance = _availableBalance #баланс
     @classification   = _classification   #природа
-    @array_card  =[Card.new(_carrierName), Card.new(_carrierName2) ]          #карты
+    @array_card       = [Card.new(_carrierName), Card.new(_carrierName2)]        #карты
     @transaction      = Transaction.new("31.12.2020","Магазин",-32)      #транзакции
+  end
+  def addCard(_carrierName)
+    @array_card << Card.new(_carrierName)
   end
   def exec
     puts @nameAccaunt, @currency, @availableBalance, @classification
-    @transaction.exec
-    for item in @array_card do
-      puts  item.exec # не подсвечивает метод
-    end
+    #@transaction.exec
+    #for item in @array_card do
+    #  puts  item.exec # не подсвечивает метод
+    #end
   end
 end
 
@@ -54,8 +57,6 @@ browser.goto "https://demo.bendigobank.com.au/banking/sign_in"
 browser.button(:name => "customer_type").click
 #копируем необходимые данные
 strct = browser.script(:id => "data").innertext
-
-puts strct
 #Находим данные
 pos1 = strct.rindex(/__DATA__/)
 
@@ -64,13 +65,27 @@ pos2 = strct.rindex(/__BOOTSTRAP_I18N__/)
 pos1 =pos1+10
 pos2 =pos2-69
 # Выделяем строку с данными
-strct1 = strct.slice(pos1,pos2)
-puts strct1
+strct = strct.slice(pos1,pos2)
+puts strct
 #здесь при парсинг JSON
-my_hash = JSON.parse(strct1)
+my_hash = JSON.parse(strct)
 
-# пример того как будет заполнятся оьект класса
-accaunt = Accaunt.new("DailyAcc", "MDL", 1213123123,"Debit","Anton Pirojkov","Ivan Ivanich")
-accaunt.exec
 puts "------------------------------------------------------------------------------------"
-puts my_hash # получение данных в my_hash
+# создаём оьект класса
+array_accaunts = Array.new
+for item in my_hash["accounts"] do
+  array_accaunts << Accaunt.new(my_hash["accounts"][0]["name"], my_hash["accounts"][0]["currentBalance"]["currency"], my_hash["accounts"][0]["currentBalance"]["value"],my_hash["accounts"][0]["classification"],my_hash["accounts"][0]["cards"][0]["carrierName"],my_hash["accounts"][0]["cards"][1]["carrierName"])
+end
+
+for item in array_accaunts do
+  puts  item.exec # не подсвечивает метод
+  puts "+++++++++++++++++++++++++++++++++++++++++++"
+end
+
+#array_accaunts << Accaunt.new(my_hash["accounts"][0]["name"], my_hash["accounts"][0]["currentBalance"]["currency"], my_hash["accounts"][0]["currentBalance"]["value"],my_hash["accounts"][0]["classification"],"Anton Pirojkov","Ivan Ivanich")
+#array_accaunts[0].exec
+#for item in my_hash["accounts"] do
+#  puts  item["name"] # не подсвечивает метод
+#end
+puts "------------------------------------------------------------------------------------"
+#thismonthlist[:Roles].flat_map { |role| role[:admins] }
