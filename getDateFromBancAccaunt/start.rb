0071a655b6a5797cb58259fe8d00e1cd2bf2cd96
SSerghei <serzh.shchepanovskiy@gmail.com>
2020-09-15 17:34:16 +0300

    Добавил сериализацию обьекта в JSON

--- getDateFromBancAccaunt/start.rb
+++ getDateFromBancAccaunt/start.rb
@@ -18,6 +18,7 @@ class Transaction
   def exec
     puts @date, @description, @amount
   end
+
 end
 
 class Card
@@ -25,6 +26,11 @@ class Card
   def initialize(_carrierName)
     @carrierName        = _carrierName       #имя владельца
   end
+  def to_h
+    {
+        carrierName: carrierName
+    }
+    end
   def exec
     puts @carrierName
   end
@@ -37,19 +43,23 @@ class Accaunt
     @availableBalance  = _availableBalance #баланс
     @classification    = _classification   #природа
     @array_card        = Array.new         #карты
-    #@array_transaction = Array.new         #транзакции
+  end
+  def to_h
+      {
+          nameAccaunt: nameAccaunt,
+          currency: currency,
+          availableBalance: availableBalance,
+          classification: classification,
+          array_card: array_card.map(&:to_h)
+      }
   end
   # Добавляем карту
   def addCard(_carrierName)
     @array_card << Card.new(_carrierName)        #push карты
   end
-  #Добавляем транзакцию
-  #def addTransaction(_date, _description, _amount)
-  #  @array_transaction << Transaction.new(_date, _description, _amount)        #push карты
-  #end
+
   def exec
     puts @nameAccaunt, @currency, @availableBalance, @classification
-    #@transaction.exec
     puts "===================="
     for item in @array_card do
       puts  item.exec # не подсвечивает метод
@@ -57,63 +67,37 @@ class Accaunt
     puts "===================="
   end
 end
+# добавляем новые элементы массива из JSON hash
+array_accaunts = Array.new()
 
-class AccauntsArray
-  def initialize
-    @Accaunts        = Array.new         #карты
-  end
-  def addAccaunt(_nameAccaunt, _currency, _availableBalance, _classification)
-    @Accaunts << Accaunt.new(_nameAccaunt, _currency, _availableBalance, _classification)        #push карты
-  end
-
-  # добавляем новые элементы массива из JSON hash
-  def addAccauntFromHash
-    browser = Watir::Browser.new :chrome
-    #заходим на сайт
-    browser.goto "https://demo.bendigobank.com.au/banking/sign_in"
-    #кликаем на кнопку входа
-    browser.button(:name => "customer_type").click
-    #копируем необходимые данные
-    strct = browser.script(:id => "data").innertext
-    #Находим данные
-    pos1 = strct.rindex(/__DATA__/)
-    pos2 = strct.rindex(/__BOOTSTRAP_I18N__/)
-    pos1 =pos1+10
-    pos2 =pos2-69
-    # Копируем подстроку с данными
-    strct = strct.slice(pos1,pos2)
-    #здесь при парсинг JSON
-    my_hash = JSON.parse(strct)
-    i=0
-    for item in my_hash["accounts"] do
-      self.addAccaunt(item["name"], item["currentBalance"]["currency"], item["currentBalance"]["value"],item["classification"])
-      for card in item["cards"] do
-        @Accaunts[i].addCard(card["carrierName"])
-      end
-    i=i+1
+  browser = Watir::Browser.new :chrome
+  #заходим на сайт
+  browser.goto "https://demo.bendigobank.com.au/banking/sign_in"
+  #кликаем на кнопку входа
+  browser.button(:name => "customer_type").click
+  #копируем необходимые данные
+  strct = browser.script(:id => "data").innertext
+  #Находим данные
+  pos1 = strct.rindex(/__DATA__/)
+  pos2 = strct.rindex(/__BOOTSTRAP_I18N__/)
+  pos1 =pos1+10
+  pos2 =pos2-69
+  # Копируем подстроку с данными
+  strct = strct.slice(pos1,pos2)
+  #здесь при парсинг JSON
+  my_hash = JSON.parse(strct)
+  i=0
+  for item in my_hash["accounts"] do
+    array_accaunts << Accaunt.new(item["name"], item["currentBalance"]["currency"], item["currentBalance"]["value"],item["classification"])
+    for card in item["cards"] do
+      array_accaunts[i].addCard(card["carrierName"])
     end
+    i=i+1
   end
-end
-
-def to_hash2(obj1)
-  Hash[obj1.instance_variables.map { |key|
-    variable = obj1.instance_variable_get key
-    [key.to_s[1..-1].to_sym,
-     if variable.is_a?(Hash) then # проблемное место не понмаю как отличить Hash от обьекта и какието странные адреса в Хэше
-       to_hash2(variable)
-     else
-       variable
-     end
-    ]
-  }]
-end
-
-accaunts = AccauntsArray.new()
-accaunts.addAccauntFromHash
 
 puts "------------------------------------------------------------------------------------"
-hash = to_hash2(accaunts)
+js =array_accaunts.map(&:to_h).to_json
 
-puts hash
+puts js
 puts "------------------------------------------------------------------------------------"
 
