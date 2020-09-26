88506e8daab25778aa9c7acae2b07dfb9d7c5fed
SSerghei <serzh.shchepanovskiy@gmail.com>
2020-09-26 03:13:49 +0300

    с помошью wati и xpath беру данные транзакции и помещаю в массив классов где храню все данные пока реализованна запись 10-ти первых в списке транзакций в массив также работает вывод данных обьектов в JSON

--- getDateFromBancAccaunt/start.rb
+++ getDateFromBancAccaunt/start.rb
@@ -10,14 +10,16 @@ require 'json'
 require 'jsonapi-serializers'
 
 class Transaction
-  attr_accessor :date, :description, :amount # открываем доступ r/w
-  def initialize(v_date, v_description, v_amount)
+  attr_accessor :name, :date, :description, :amount # открываем доступ r/w
+  def initialize(v_name, v_date, v_description, v_amount)
+    @name        = v_name,        #название
     @date        = v_date         #Дата
     @description = v_description  #описание
     @amount      = v_amount       #сумма
   end
   def to_h
     {
+        name: name,
         date: date,
         description: description,
         amount: amount
@@ -36,6 +38,7 @@ class Card
     }
   end
 end
+
 class Accaunt
   attr_accessor :name_accaunt, :currency, :availableBalance, :classification, :array_card,:array_transaction
   def initialize(v_nameAccaunt, v_currency, v_availableBalance, v_classification)
@@ -60,20 +63,22 @@ class Accaunt
   def addCard(v_carrierName)
     @array_card << Card.new(v_carrierName)        #push карты
   end
-
-  def exec
-    puts @nameAccaunt, @currency, @availableBalance, @classification
-    puts "===================="
-    for item in @array_card do
-      puts  item.exec # не подсвечивает метод
-    end
-    puts "===================="
+  #Добавить транзакцию
+  def add_transaction(v_name, v_date, v_description, v_amount)
+    array_transaction << Transaction.new(v_name, v_date, v_description, v_amount)        #push карты
   end
 end
+
+
 # добавляем новые элементы массива из JSON hash
 array_accaunts = Array.new()
 browser = Watir::Browser.new :firefox
-
+def scroll_to_bottom(_browser)
+  #скролим вниз сайта чтобы можно было бы подцепить весь список транзакций
+  while _browser.text.include?("No more activity")==false do
+    _browser.scroll.to :bottom
+  end
+end
 
 #заходим на сайт
 browser.goto "https://demo.bendigobank.com.au/banking/sign_in"
@@ -83,41 +88,6 @@ browser.button(:name => "customer_type").click
 #копируем необходимые данные
 strct = browser.script(:id => "data").innertext
 
-#doc =Nokogiri::HTML(browser.html)
-
-browser.elements(:xpath, '//li[@data-semantic="account-item"]').map do |build|
-  build.click
-  # Watir::Wait.until { ... }
-  #скролим вниз сайта чтобы можно было бы подцепить весь список транзакций
-  while browser.text.include?("No more activity")==false do
-    browser.scroll.to :bottom
-  end
-  #кликаем поочерёдно по всем транзакциям
-  browser.elements(:xpath, '//a[@class="_1pyzXOL8PW panel--hover"]').map do |transaction|
-     transaction.click
-     sleep 3
-     #берём значения со страницы транзакции
-     #собираем данные с транзакции
-     #name_transaction = browser.h2(class: 'overflow-ellipsis panel__header__label__primary').text
-     #puts name_transaction
-     #amount_transaction = browser.span(class: 'overflow-ellipsis amount').text
-     #puts amount_transaction
-     #description_transaction = browser.span(class: 'uilist__item__label').text
-     #puts description_transaction
-     #date_transaction = browser.span(class: 'uilist__item__detail').text
-     #puts date_transaction
-     #возвращаемся назад к списку транзакции
-     #
-     browser.back
-     #скролим вниз сайта чтобы можно было бы кликнуть по любой транзакции
-     while browser.text.include?("No more activity")==false do
-       browser.scroll.to :bottom
-     end
-  end
-end
-
-
-puts "------------------------------------------------------------------------------------"
 #Находим данные
 pos1 = strct.rindex(/__DATA__/)
 pos2 = strct.rindex(/__BOOTSTRAP_I18N__/)
@@ -135,6 +105,57 @@ i=0
     end
     i=i+1
  end
+
+#TWO_MONTH                  = 60
+#current_date               = Time.new.strftime('%Y-%m-%d')
+#edge_date                  = Date.strptime(current_date) - TWO_MONTH
+header_transaction_span    = '//span[@data-semantic="payment-amount"]'
+properties_transaction_div = '//div[@class="uilist__item"]'
+#transaction_li             = '//li[contains(@class, "grouped-list__group") and contains(@data-semantic, "activity-group")]'
+#list_transaction_li        = './/li[@data-semantic="activity-item"]'
+description_transaction_tag    = '//span[@class="uilist__item__detail"]'
+i = 0
+browser.elements(:xpath, '//li[@data-semantic="account-item"]').map do |build|
+  if i < 4 then
+    build.click
+    sleep 10
+    i+=1
+  else
+    break
+  end
+  # begin
+  # transaction_date = Date.strptime(list_transaction.values.last)
+
+  #скролим вниз
+  #scroll_to_bottom(browser)
+  j = 0
+  browser.elements(:xpath, '//a[@class="_1pyzXOL8PW panel--hover"]').map do |transaction|
+    if j < 10 then
+      #кликаем по очерёдной транзакции
+      transaction.click
+      sleep 1
+      j += 1
+    else
+      break
+    end
+    #собираем данные с транзакции
+    header_transaction = browser.element(:xpath, header_transaction_span).text
+
+    properties_transaction = browser.element(:xpath,properties_transaction_div)
+
+    name_transaction        = properties_transaction.elements(:xpath,'//span[@class="uilist__item__label"]')[0].text
+    date_transaction        = properties_transaction.elements(:xpath,'//span[@class="uilist__item__detail"]')[0].text
+    description_transaction = properties_transaction.elements(:xpath,description_transaction_tag)[2].text
+    amount_transaction = header_transaction.delete('Credit of ')
+    array_accaunts[i-1].add_transaction(name_transaction, date_transaction, description_transaction, amount_transaction)
+
+    browser.back
+    #скролим вниз
+    #scroll_to_bottom(browser)
+  end
+
+end
+#while transaction_date < edge_date do
 
 puts "------------------------------------------------------------------------------------"
 js =array_accaunts.map(&:to_h).to_json
